5381c7ea162927173e0c46c4d1bc5568be33852f
Majo <majocarbajal@gmail.com>
2023-05-08 18:35:18 -0300

    change instruments every two seconds while playing midi

--- Mido/main.py
+++ Mido/main.py
@@ -1,21 +1,41 @@
 import mido
+import time
 
-# Load the MIDI file
-mid = mido.MidiFile('C:/Users/majoc/OneDrive/Documents/Processing/sketch_230508c/billyjean.mid')
+# Set up MIDI output port
+output = mido.open_output()
 
-# Select the instrument (program) you want to use
-program = 22  # 0 Piano ; 22 Harmonica
+# Load MIDI file
+#mid = mido.MidiFile('C:/Users/majoc/OneDrive/Documents/Processing/sketch_230508c/billyjean.mid')
+mid = mido.MidiFile("magenta_2023-03-19_212732_009.mid")
 
-# Iterate over all messages in all tracks
-for i, track in enumerate(mid.tracks):
-    for msg in track:
-        # Check if the message is a program change message
-        if msg.type == 'program_change':
-            # Set the new program (instrument)
-            msg.program = program
+# Define instruments
+piano = 0
+accordion = 21
 
-# Play the modified MIDI file
-outport = mido.open_output()
-for message in mid.play():
-    outport.send(message)
+# Set initial instrument
+current_instrument = piano
+
+# Define timer variables
+interval = 2  # Time in seconds between instrument changes
+last_time = time.time()
+
+# Iterate through MIDI file
+for msg in mid.play():
+
+    # Check if it's time to switch instruments
+    if time.time() - last_time >= interval:
+        last_time = time.time()
+
+        # Switch instrument
+        if current_instrument == piano:
+            current_instrument = accordion
+        else:
+            current_instrument = piano
+
+        # Send program change message to MIDI output
+        output.send(mido.Message('program_change', program=current_instrument))
+
+    # Send MIDI message to output
+    output.send(msg)
+
 
