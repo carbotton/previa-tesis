6ff2cfb3513b4e213dcd6354dc5e435e354a056e
Majo <majocarbajal@gmail.com>
2023-05-14 12:22:38 -0300

    Code explained line by line

--- POC-1/main.py
+++ POC-1/main.py
@@ -1,10 +1,17 @@
-import mediapipe as mp
+import mediapipe as mp  # for hand tracking
 import numpy as np
 import pyaudio
-import cv2
+import cv2  # for image processing
 
 
 def map_to_a_minor(pitch):
+    """
+    This is a function that maps a given pitch to the nearest pitch in the A minor scale.
+    The function takes a pitch value as input and calculates its pitch class (which is just the pitch modulo 12).
+    It then finds the closest pitch class in the A minor scale, calculates the corresponding pitch in that scale,
+    and returns it.
+    """
+
     # Calculate pitch class
     pitch_class = (pitch - 21) % 12
 
@@ -28,13 +35,13 @@ mp_hands = mp.solutions.hands
 hands = mp_hands.Hands(min_detection_confidence=0.7, min_tracking_confidence=0.5)
 
 # Setup sine wave parameters
-frequency = 440  # starting frequency
-sample_rate = 44100
+frequency = 440  # starting frequency. Standard tuning pitch
+sample_rate = 44100  # 44.1 kHz standard CD-quality audio
 duration = 1  # duration of each sine wave sample in seconds
-t = np.linspace(0, duration, int(sample_rate * duration), False)
+t = np.linspace(0, duration, int(sample_rate * duration), False)  # represents the time values for each sample
 
 # Setup webcam
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(0)  # set up computer's webcam as video source for hand tracking
 
 # Load image to overlay
 overlay_img = cv2.imread('source/mandelbrot.jpg', cv2.IMREAD_UNCHANGED)
@@ -42,30 +49,31 @@ overlay_img = cv2.resize(overlay_img, (1280, 720))
 
 while True:
     # Get camera frame
-    ret, frame = cap.read()
+    ret, frame = cap.read()  # ret:bool indicates whether a frame was successfully read. frame: actual image data.
     if not ret:
         break
 
     # Get hand landmarks
     results = hands.process(frame)
     if results.multi_hand_landmarks:
-        hand_landmarks = results.multi_hand_landmarks[0]
+        hand_landmarks = results.multi_hand_landmarks[0]  # first hand is selected
 
-        # Get position of index finger
+        # Get position of index finger and map to corresponding pixel coordinates in camera frame
         index_finger_landmark = hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP]
         x, y = int(index_finger_landmark.x * frame.shape[1]), int(index_finger_landmark.y * frame.shape[0])
 
-        # Map finger position to color
+        # Map finger position to color and draw small circle at that position in camera frame
         overlay_color = tuple(map(int, overlay_img[y, x]))
         cv2.circle(frame, (x, y), 5, overlay_color, -1)
 
         # Map color to pitch
         r, g, b,  = overlay_color
-        hue = (r + g + b) / 3
+        hue = (r + g + b) / 3  # average hue value of overlay color
         pitch = int(np.interp(hue, [0, 255], [100, 2000]))
-        pitch=map_to_a_minor(pitch)
+        pitch = map_to_a_minor(pitch)
+
         # Generate sine wave
-        frequency = 440 * (2 ** ((pitch - 1000) / 1200))  # convert pitch to frequency
+        frequency = 440 * (2 ** ((pitch - 1000) / 1200))  # convert pitch to frequency in Hz. Formula for equal temperament tuning
         sine_wave = np.sin(2 * np.pi * frequency * t).astype(np.float32)
 
         # Play sine wave
@@ -73,7 +81,6 @@ while True:
 
         # Draw finger position and overlay image on frame
         mp_drawing.draw_landmarks(frame, hand_landmarks, mp_hands.HAND_CONNECTIONS)
-
         cv2.putText(frame, 'Index finger', (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2, cv2.LINE_AA)
 
     alpha = 0.5
@@ -82,17 +89,17 @@ while True:
     # print(frame.shape)  # should be (height, width, num_channels)
     overlay_img = cv2.resize(overlay_img, (frame.shape[1], frame.shape[0])) # esta linea es porque los sizes daban (720, 1280, 3) y (480, 640, 3) y eso daba error en cv2.addWeighted()
 
-    cv2.addWeighted(overlay_img, alpha, frame, 1 - alpha, 0, frame)
-    cv2.imshow('Hand Tracking', frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    cv2.addWeighted(overlay_img, alpha, frame, 1 - alpha, 0, frame)  # blends 2 images; result stored in frame
+    cv2.imshow('Hand Tracking', frame)  # display frame image in a window titled Hand Tracking
+    if cv2.waitKey(1) & 0xFF == ord('q'):  # wait for key press for 1 second. If 'q' is pressed, programm exits.
         break
 
 
 # Clean up
-cap.release()
-hands.close()
+cap.release()  # release video capture object cap and free up any resources being used
+hands.close()  # release HandTracker object and free up any resources being used
 stream.stop_stream()
 stream.close()
 p.terminate()
-cv2.destroyAllWindows()
+cv2.destroyAllWindows() 
 
